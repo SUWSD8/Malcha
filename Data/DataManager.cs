e4ba7dad46c2042164118c4eab5c3d153e2d0631
kdy0618 <kdy0618@suwon.ac.kr>
2026-05-21 10:34:12 +0900

    feat : 폴더 구조 및 데이터 모델/매니저 클래스 추가
    Controller, Repository 폴더를 프로젝트에 추가하고, 데이터 관리를 위한 DataManager 싱글턴 클래스와 Frame, TrainedData 모델 클래스를 새로 도입하였습니다. DataManager는 카탈로그 읽기 및 학습 데이터 반환 기능을 포함합니다.

---
+++ Data/DataManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Malcha.Model;
+
+namespace Malcha.Data
+{
+    internal class DataManager
+    {
+        private static readonly DataManager _instance = new DataManager();
+
+        public static DataManager Instance
+        {
+            get { 
+                return _instance;
+            }
+        }
+
+        private DataManager()
+        {
+            // Private constructor to prevent instantiation
+        }
+
+        private List<Frame> ReadCatalog()
+        {             // Implement logic to read the catalog and return a list of Frame objects
+            return new List<Frame>();
+        }
+
+        private List<TrainedData> Train(){
+            return new List<TrainedData>();
+        }
+    }
+}
---
+++ Model/Frame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Malcha.Model
+{
+    internal class Frame
+    {
+        private string id { get; set; }
+        private long timestamp { get; set; }
+        private string imagePath { get; set; }
+        private double angle { get; set; }
+        private string model { get; set; }
+        private double throttle { get; set; }
+
+        public Frame(string id, long timestamp, string imagePath, double angle, string model, double throttle)
+        {
+            this.id = id;
+            this.timestamp = timestamp;
+            this.imagePath = imagePath;
+            this.angle = angle;
+            this.model = model;
+            this.throttle = throttle;
+        }
+
+       
+    }
+}
